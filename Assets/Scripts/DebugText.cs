bf6e78590c2f0ed3e76a4a0392a8aa7386876d5f
Ivan Gnusarkov <ivankapitan@live.ru>
2020-04-25 00:08:09 +0700

    всякие утилитки

---
+++ Assets/Scripts/DebugText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugText : MonoBehaviour {
+    private static DebugText INSTANCE;
+    private Text textComponent;
+
+    public static void debug(string text) {
+        INSTANCE.debugInternal(text);
+    }
+
+    private void Start() {
+        INSTANCE = this;
+        textComponent = GetComponent<Text>();
+    }
+
+    private void debugInternal(string text) {
+        textComponent.text = text;
+    }
+}

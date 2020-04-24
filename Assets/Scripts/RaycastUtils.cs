2ae0e52757b594c38692a932255107d329d62afe
Ivan Gnusarkov <ivankapitan@live.ru>
2020-04-24 23:29:38 +0700

    test

---
+++ Assets/Scripts/RaycastUtils.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public static class RaycastUtils {
+    public static Vector3? raycastPosition(Camera camera) {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit)) {
+            return hit.point;
+        }
+
+        return null;
+    }
+
+    public static Vector3? raycastPositionWith(Camera camera, float? x = null, float? y = null, float? z = null) {
+        Vector3? position = raycastPosition(camera);
+        if (!position.HasValue) {
+            return null;
+        }
+        Vector3 result = position.Value;
+
+        if (x.HasValue) result.x = x.Value;
+        if (y.HasValue) result.y = y.Value;
+        if (z.HasValue) result.z = z.Value;
+        return result;
+    }
+}

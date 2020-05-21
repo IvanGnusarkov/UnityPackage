b58527543b1b83960f60da1fe7f7e030c89f6ec1
Ivan Gnusarkov <ivankapitan@live.ru>
2020-05-22 00:23:46 +0700

    улучшил resetter

--- Assets/IvanScripts/Resetter.cs
+++ Assets/IvanScripts/Resetter.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using IvanScripts;
 using IvanScripts.Lang;
 using UnityEngine;
 
 internal struct ObjectInfo {
     public GameObject go;
-    public Vector3 position;
-    public Quaternion rotation;
+    public Vector3 localPosition;
+    public Quaternion localRotation;
+    public Vector3 localScale;
 }
 
 public class Resetter: Singleton<Resetter> {
@@ -20,39 +22,60 @@ public class Resetter: Singleton<Resetter> {
             Transform oTransform = resettableObject.transform;
             infos.Add(new ObjectInfo {
                 go = resettableObject,
-                position = oTransform.position,
-                rotation = oTransform.rotation
+                localPosition = oTransform.localPosition,
+                localRotation = oTransform.localRotation,
+                localScale = oTransform.localScale
             });
         });
     }
 
-    public void resetEverything() {
-        infos.ForEach(info => {
-            GameObject go = info.go;
-            Transform goTransform = go.transform;
-            goTransform.position = info.position;
-            goTransform.rotation = info.rotation;
+    public void resetInstantlyEverything() {
+        infos.ForEach(resetInstantlyByInfo);
+    }
 
-            Rigidbody goBody = go.GetComponent<Rigidbody>();
-            if (goBody != null) {
-                goBody.velocity = Vector3.zero;
-                goBody.angularVelocity = Vector3.zero;
-            }
-        });
+    public void resetInstantly(GameObject go) {
+        resetInstantlyByInfo(find(go));
+    }
+
+    public void resetSmoothly(GameObject go, float delay, float duration) {
+        resetSmoothlyByInfo(find(go), delay, duration);
     }
 
     public void resetSmoothly(float delay, float duration) {
-        StartCoroutine(resetSmoothlyCoroutine(delay, duration));
+        StartCoroutine(CoroutineUtils.delay(delay, () => {
+            foreach (ObjectInfo info in infos) {
+                Transform tr = info.go.transform;
+                tr.DOMove(info.localPosition, duration);
+                tr.DORotate(info.localRotation.eulerAngles, duration);
+                tr.DOScale(info.localScale, duration);
+            }
+        }));
     }
 
-    private IEnumerator resetSmoothlyCoroutine(float delay, float duration) {
-        yield return new WaitForSecondsRealtime(delay);
-        foreach (ObjectInfo info in infos) {
-            Transform tr = info.go.transform;
-            tr.DOMove(info.position, duration);
-            tr.DORotate(info.rotation.eulerAngles, duration);
-        }
+    private void resetInstantlyByInfo(ObjectInfo info) {
+        GameObject go = info.go;
+        Transform goTransform = go.transform;
+        goTransform.localPosition = info.localPosition;
+        goTransform.localRotation = info.localRotation;
+        goTransform.localScale = info.localScale;
 
-        yield return null;
+        Rigidbody goBody = go.GetComponent<Rigidbody>();
+        if (goBody != null) {
+            goBody.velocity = Vector3.zero;
+            goBody.angularVelocity = Vector3.zero;
+        }
+    }
+
+    private void resetSmoothlyByInfo(ObjectInfo info, float delay, float duration) {
+        StartCoroutine(CoroutineUtils.delay(delay, () => {
+            Transform tr = info.go.transform;
+            tr.DOMove(info.localPosition, duration);
+            tr.DORotate(info.localRotation.eulerAngles, duration);
+            tr.DOScale(info.localScale, duration);
+        }));
+    }
+
+    private ObjectInfo find(GameObject go) {
+        return infos.Find(storedInfo => storedInfo.go == go);
     }
 }
